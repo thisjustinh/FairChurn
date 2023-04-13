cac573f1bdb7814095aca608632a9c0383820f6d
Huang, Justin <jfh5730@psu.edu>
2023-04-13 16:59:04 -0400

    Add code for adversarial predictor for debiasing. Still need to check for fairness on age though.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FairChurn
 Predicting bank customer churn while mitigating bias on age
+
+## References
+
+Zhang, B. H., Lemoine, B., & Mitchell, M. (2018, December). Mitigating unwanted biases with adversarial learning. In Proceedings of the 2018 AAAI/ACM Conference on AI, Ethics, and Society (pp. 335-340).  
---
+++ src/fairnn.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+
+class FairAdversary(nn.Module):
+    """
+    Create adversary for debiasing as proposed in Zhang et al.
+    Adversary seeks to predict the sensitive attribute given the classifier output
+    """
+    def __init__(self, n_out, n_hidden):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(1, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, n_out),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+        return self.model(x)
